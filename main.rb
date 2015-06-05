7428dbdbdd85b3969c06e65031b30892ce4b0e75
Sander Verkuil <saverkuil@gmail.com>
2015-06-05 20:11:01 +0200

    IT will now parse every file in /dataset/maildir/**/sent/* :smiley:

--- main.rb
+++ main.rb
@@ -1,5 +1,29 @@
 require 'neo4j'
+require 'neo4j-core'
 require 'mail'
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def pink
+    colorize(35)
+  end
+end
 
 class UserEmail
   include Neo4j::ActiveRel
@@ -9,8 +33,8 @@ class UserEmail
 
   type "sent_mail"
 
-  property :subject, type:String
-  property :date, type:DateTime
+  property :amount, type:Integer
+
 end
 
 class User
@@ -21,13 +45,9 @@ class User
   property :name, type:String
   property :mail, type:String
 
-  has_many :out, :sentmails, rel_class: UserEmail
-  has_many :in, :receivedmails, rel_class: UserEmail
+  has_many :out, :sentmails, rel_class: UserEmail, unique: true, model_class: User
+  has_many :in, :receivedmails, rel_class: UserEmail, unique: true, model_class: User
 
-end
-
-def createUser(email)
-  createUser(email[/(.*?)@/,1].gsub(/[^\w\s\d]/, ' ').split.map(&:capitalize).join(' '), email)
 end
 
 def createUser(name, email)
@@ -36,13 +56,55 @@ def createUser(name, email)
 end
 
 def createMail(mail)
-  mail.header['X-To'].to_s.split(', ').zip(mail.to).each do |to|
-    puts createUser to[0], to[1]
+  toArray ||= Array.new
+  xto = mail.header['X-To'].to_s.split(', ')
+  if (mail.to && mail.to.any?)
+    mail.to.each do |to|
+      toArray << createUser(to[/(.*?)@/,1].gsub(/[^\w\s\d]/, ' ').split.map(&:capitalize).join(' '), to)
+    end
+  elsif xto && xto.any?
+    xto.each do |to|
+      toArray << createUser(to[/(.*?)@/,1].gsub(/[^\w\s\d]/, ' ').split.map(&:capitalize).join(' '), to)
+    end
   end
+
+
+  fromArray ||= Array.new
+  mail.from.each do |from|
+    fromArray << createUser(from[/(.*?)@/,1].gsub(/[^\w\s\d]/, ' ').split.map(&:capitalize).join(' '), from)
+  end
+
+  toArray.each do |user|
+    fromArray.each do |from|
+      if (from.sentmails.include?(user))
+        from.rels(dir: :outgoing, between: user).each do |rel|
+          rel.amount += 1
+          rel.save
+        end
+      else
+        UserEmail.create(from_node: from, to_node: user, amount: 1)
+      end
+    end
+  end
+
+  return {:fromCount => fromArray.count, :toCount => toArray.count}
+
 end
 
 Neo4j::Session.open(:server_db)
 
-mail = Mail.read("dataset/maildir/allen-p/sent/1")
+limit = 1000;
 
-createMail mail
+Dir.glob("dataset/maildir/**/sent/*") do |file|
+  added = createMail(Mail.read(file))
+
+  uname, nomail = file.match(/dataset\/maildir\/(.*)\/sent\/(\d*)/).captures
+
+  first = "#{uname} - #{nomail}\t=>".yellow
+  to = "#{added[:toCount]} users".green
+  from = "#{added[:fromCount]} users".green
+
+  puts "#{first} Parsed #{from} that sent the mail, and #{to} that received the mail"
+
+end
+
